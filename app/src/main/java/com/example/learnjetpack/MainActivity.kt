87aa3bdf3c2b3504c04404f392b0ce9240ba3595
licoba <licoba@163.com>
2021-08-05 14:05:44 +0800

    1

--- app/src/main/java/com/example/learnjetpack/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpack/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.learnjetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycle.addObserver(MyObserver()) // lifecycle 这里的lifecycle实际上是getlifecycle
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(javaClass.name, "-------onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(javaClass.name, "-------onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(javaClass.name, "-------onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(javaClass.name, "-------onStop")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(javaClass.name, "-------onDestroy")
+    }
+
 }
---
+++ app/src/main/java/com/example/learnjetpack/MyObserver.kt
@@ -0,0 +1,44 @@
+package com.example.learnjetpack
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class MyObserver : LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStart() {
+        Log.e(javaClass.name, "-------onStart")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate() {
+        Log.e(javaClass.name, "-------onCreate")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume() {
+        Log.e(javaClass.name, "-------onResume")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPause() {
+        Log.e(javaClass.name, "-------onPause")
+    }
+
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStop() {
+        Log.e(javaClass.name, "-------onStop")
+    }
+
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+        Log.e(javaClass.name, "-------onDestroy")
+    }
+
+}
+
+
